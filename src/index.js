50589719524737227d11e1fe700bf37fdec1d2d1
kk0ktgw <76461553+kk0ktgw@users.noreply.github.com>
2020-12-23 12:43:54 +0900

    lesson

--- src/index.js
+++ src/index.js
@@ -13,11 +13,41 @@ const onclickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
+  //完了ボタンを追加
+  const completeButton = document.createElement("button");
+  completeButton.innerText = "完了";
+  completeButton.addEventListener("click", () => {
+    alert("完了");
+    //押された完了ボタンの親要素のdivタグをcomplete-listに移動する
+    deleteFromICList(completeButton.parentNode);
+    //   const deleteTarget = completeButton.parentNode;
+    //   document.getElementById("imcomplete-list").removeChild(deleteTarget);
+  });
+
+  //削除ボタンを追加
+  const deleteButton = document.createElement("button");
+  deleteButton.innerText = "削除";
+  deleteButton.addEventListener("click", () => {
+    alert("削除");
+    //押された削除ボタンの親要素のdivタグを削除
+    deleteFromICList(deleteButton.parentNode);
+    // const deleteTarget = deleteButton.parentNode;
+    // document.getElementById("imcomplete-list").removeChild(deleteTarget);
+  });
+
   //divタグの子要素を設定
   div.appendChild(li);
+  div.appendChild(completeButton);
+  div.appendChild(deleteButton);
 
   //未完了のリストに追加していく
   document.getElementById("imcomplete-list").appendChild(div);
+
+  //未完了リストから指定の要素を削除
+  const deleteFromICList = (target) => {
+    const deleteTarget = deleteButton.parentNode;
+    document.getElementById("imcomplete-list").removeChild(deleteTarget);
+  };
 };
 
 document
