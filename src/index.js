192014398f24d41fc086a38c1e955a033bb39d19
kk0ktgw <76461553+kk0ktgw@users.noreply.github.com>
2020-12-23 14:35:46 +0900

    lesson

--- src/index.js
+++ src/index.js
@@ -13,22 +13,62 @@ const onclickAdd = () => {
   const li = document.createElement("li");
   li.innerText = inputText;
 
-  //完了ボタンを追加
+  //完了ボタン生成
   const completeButton = document.createElement("button");
   completeButton.innerText = "完了";
   completeButton.addEventListener("click", () => {
-    alert("完了");
-    //押された完了ボタンの親要素のdivタグをcomplete-listに移動する
+    // alert("完了");
     deleteFromICList(completeButton.parentNode);
     //   const deleteTarget = completeButton.parentNode;
     //   document.getElementById("imcomplete-list").removeChild(deleteTarget);
+
+    //完了リストに追加する要素
+    const addTarget = completeButton.parentNode;
+
+    //divの1番目子要素のテキスト要素を取得
+    const text = addTarget.firstElementChild.innerText;
+
+    //div以下を初期化
+    addTarget.textContent = null;
+    const li = document.createElement("li");
+    li.innerText = text;
+
+    //戻すボタン生成
+    const backButton = document.createElement("button");
+    backButton.innerText = "戻す";
+    backButton.addEventListener("click", () => {
+      //押された戻すボタンの親要素のdivタグを削除
+      const deleteTarget = backButton.parentNode;
+      document.getElementById("complete-list").removeChild(deleteTarget);
+      //未完了リストに追加する要素
+      const addTarget2 = backButton.parentNode;
+      //divの最初の子要素のテキスト要素を取得
+      const text2 = addTarget2.firstElementChild.innerText;
+      //div以下を初期化
+      addTarget2.textContent = null;
+      const li2 = document.createElement("li");
+      li2.innerText = text2;
+      //divタグに各要素を設定
+      addTarget2.appendChild(li2);
+      addTarget2.appendChild(completeButton);
+      addTarget2.appendChild(deleteButton);
+      //未完了リストに追加する
+      document.getElementById("imcomplete-list").appendChild(addTarget2);
+    });
+
+    //divタグの子要素に各要素を設定
+    addTarget.appendChild(li);
+    addTarget.appendChild(backButton);
+
+    //完了リストに追加する
+    document.getElementById("complete-list").appendChild(addTarget);
   });
 
   //削除ボタンを追加
   const deleteButton = document.createElement("button");
   deleteButton.innerText = "削除";
   deleteButton.addEventListener("click", () => {
-    alert("削除");
+    // alert("削除");
     //押された削除ボタンの親要素のdivタグを削除
     deleteFromICList(deleteButton.parentNode);
     // const deleteTarget = deleteButton.parentNode;
